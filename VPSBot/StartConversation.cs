4e1d891eac01ae8c8f7e7723bb237166a219a22b
Harsh Raj <sdharshraj@gmail.com>
2016-07-05 19:07:57 +0530

    Test commit

--- VPSBot/StartConversation.cs
+++ VPSBot/StartConversation.cs
@@ -32,7 +32,7 @@ namespace VPSBot
         [LuisIntent("greet")]
         public async Task Greet(IDialogContext context, LuisResult result)
         {
-            string message = result.Query + $" sir. \n How can i help you?";
+            string message = result.Query + $" sir. \n How can i help you? type anything";
             await context.PostAsync(message);
             context.Wait(MessageReceived);
         }
